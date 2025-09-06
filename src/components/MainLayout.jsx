2c65161ff84c52126c6885c55230f2d17d3d28f1
gazbena <ghazbena@gmail.com>
2025-09-06 15:09:11 -0400

    implementing main layout

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,17 +1,28 @@
 import "./MainLayout.css"
 
 import { Outlet } from "react-router-dom"
+import { useState } from "react"
 import Header from "./Header"
 import Footer from "./Footer"
+import Menu from "./Menu"
 
 export default function MainLayout() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    
+    const closeMenu = () => setIsMenuOpen(false)
+    
+    const handleMenu = () => !isMenuOpen 
+        ? setIsMenuOpen(true) 
+        : setIsMenuOpen(false)
+
     return (
-        <div className="site-wrapper">
-            <Header />
-            <main>
+        <div className="site-wrapper" style={isMenuOpen ? {overflow: "hidden"}: undefined}>
+            <Header isMenuOpen={isMenuOpen} handleMenu={handleMenu} closeMenu={closeMenu}/>
+            {isMenuOpen && <Menu closeMenu={closeMenu}/>}
+            <main className="main-layout" style={isMenuOpen ? {maxHeight: '80vh', overflow: 'hidden'}:undefined}>
                 <Outlet />
             </main>
-            <Footer />
+            <Footer isMenuOpen={isMenuOpen}/>
         </div>
     )
 }
