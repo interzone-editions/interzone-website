d073e4629ba39eae603849e518695e2aba9be7c0
gazbena <ghazbena@gmail.com>
2026-03-07 10:34:47 -0500

    feat: add not found error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,17 @@
+import { Container} from "react-bootstrap"
+import { Link } from "react-router"
 
 function NotFound() {
     return (
-        <h1>Not Found</h1>
+        <Container 
+            fluid 
+            style={{ minHeight: "68vh" }} 
+            className="d-flex flex-column align-items-center justify-content-center"
+        >
+            <h1>NOT FOUND</h1>
+            <p>The page you are looking for can not be found...</p>
+            <Link to="/" className="link">Back to home</Link>
+        </Container>
     )
 }
 
